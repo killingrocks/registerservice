db01bfd7bcf1cb677135152506c2bfad50f4a2bf
killingrocks <namarsde@email.uark.edu>
2017-07-11 09:29:28 -0500

    see

--- src/main/java/edu/uark/controllers/ProductRestController.java
+++ src/main/java/edu/uark/controllers/ProductRestController.java
@@ -41,6 +41,10 @@ public class ProductRestController {
 	@RequestMapping(value = "/apiv0/", method = RequestMethod.PUT)
 	public Product putActivity(@RequestBody Product product) {
 		System.out.println("=======================================================");
+		System.out.println("IN CONTROL [LOOK UP CODE:"+ product.getLookupCode() +"]");
+		System.out.println("=======================================================");
+		
+		System.out.println("=======================================================");
 		System.out.println("IN CONTROL [FIRST NAME:"+ product.getFirstName() +"]");
 		System.out.println("=======================================================");
 		return (new ProductSaveCommand()).
